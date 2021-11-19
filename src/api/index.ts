99c533094d7bbdf242748af3968f16c30cbacc65
yuliia-resh <yuliia.resh@gmail.com>
2021-11-19 14:39:52 +0200

    Added runnin json-server

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,24 +1,24 @@
 import axios from "axios";
 import { CartItemType, ProductType } from "../types/types";
 
-const BASE_URL = process.env.PORT;
+// const BASE_URL = process.env.PORT;
 
 export const getAllProductsApi = () => {
-  return axios.get(`${BASE_URL}/data`);
+  return axios.get(`/data`);
 };
 
 export const postToCartApi = (product: ProductType) => {
-  return axios.post(`${BASE_URL}/cart`, product);
+  return axios.post(`/cart`, product);
 };
 
 export const getCartProductsApi = () => {
-  return axios.get(`${BASE_URL}/cart`);
+  return axios.get(`/cart`);
 };
 
 export const updateCartProductByIdApi = (updatedProduct: CartItemType) => {
-  return axios.put(`${BASE_URL}/cart/${updatedProduct.id}`, updatedProduct);
+  return axios.put(`/cart/${updatedProduct.id}`, updatedProduct);
 };
 
 export const deleteCartProductApi = (productId: number) => {
-  return axios.delete(`${BASE_URL}/cart/${productId}`);
+  return axios.delete(`/cart/${productId}`);
 };
